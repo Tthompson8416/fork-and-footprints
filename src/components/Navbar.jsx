af086550492431f080add195c414a6f42ab7ace6
TT84 <tthompson8416@outlook.com>
2024-12-19 06:01:46 -0500

    Logo added to homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,28 @@
 import { Link } from "react-router-dom";
 
+
 function Navbar() {
   return (
-    <nav className="bg-custom-accent text-white text-3xl fixed top-0 left-0 w-full z-10 p-4">
-      <div className="w-full flex justify-between items-center">
-        <h1 className="text-3xl  pl-10">Fork & Footprints</h1>
+    <nav className="bg-custom-accent text-white text-3xl fixed top-0 left-0 w-full z-10 p-4 flex items-center" style={{ height: "60px" }}>
+      <div className="w-full flex items-center justify-between">
+        {/* Left side */}
+        <h1 className="text-3xl pl-10">Fork & Footprints</h1>
 
+        {/* Center logo 
+        <div className="flex justify-center items-center mt-56">
+          <img
+            src={logo}
+            alt="Fork & Footprints Logo"
+            className="mx-auto"
+            style={{
+              height: "100%", // Control logo height explicitly
+              width: "30%",  // Maintain aspect ratio
+              maxHeight: "100%", // Prevent overflow within the navbar
+            }}
+          />
+        </div> */}
+
+        {/* Right side */}
         <ul className="flex gap-10 pr-10 font-thin">
           <li>
             <Link to="/" className="hover:text-yellow-300">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import backgroundImage from "../assets/images/homepage-background.jpg";
+import logo from "../assets/images/logo.png"; // Adjust the path to your logo file
+
 
 const Home = () => {
   return (
@@ -9,8 +11,22 @@ const Home = () => {
       }}
     >
       {/* Transparent Rectangle */}
-      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-50 border-4 border-gray-200 p-10 text-center rounded-lg max-w-[80%] w-[900px] h-[420px]">
-        <h1 className="text-6xl font uppercase tracking-wider mb-4 mt-20">Fork & Footprints</h1>
+      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-50 border-4 border-gray-200 mb-8 text-center rounded-lg max-w-[80%] w-[900px] h-[500px]">
+
+      {/* Center logo */}
+      <div className="flex justify-center items-center pb-2">
+          <img
+            src={logo}
+            alt="Fork & Footprints Logo"
+            className="mx-auto mb-2"
+            style={{
+              height: "100%", // Control logo height explicitly
+              width: "28%",  // Maintain aspect ratio
+              maxHeight: "100%", // Prevent overflow within the navbar
+            }}
+          />
+        </div> 
+        <h1 className="text-5xl font uppercase tracking-wider mb-4">Fork & Footprints</h1>
         <p className="text-3xl font-thin pt-5 leading-relaxed">
           Explore the world, one bite at a time.
           <br />
