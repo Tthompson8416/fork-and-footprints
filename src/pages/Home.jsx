c9e94419e9e5d0f146e9f64c53593c2054d6286d
TT84 <tthompson8416@outlook.com>
2024-12-24 17:25:51 -0500

    RWD adjusted for xl screen size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -95,23 +95,23 @@ const Home = () => {
               <img
                 src={forkImage} // Fork icon
                 alt="Fork Icon"
-                className="h-6 w-6 sm:h-12 sm:w-12"
+                className="h-6 w-6 sm:h-12 sm:w-12 xl:w-16 xl:h-16 "
               />
             </div>
 
             {/* Transparent Rectangle */}
-            <div className="absolute max-w-[80%] w-[900px] h-[300px] top-72 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-50 border-4 border-gray-200 text-center rounded-lg xs:text-blue-700 sm:text-red-700 md:text-gray-600 lg:text-yellow-500 xl:text-green-600 2xl:text-pink-400 3xl:text-purple-900">
-              <h2 className="text-2xl sm:text-3xl uppercase tracking-wider mt-8">
+            <div className="absolute max-w-[80%] md:max-w-[60%] w-[900px] h-[300px] top-72 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-50 border-4 border-gray-200 text-center rounded-lg xs:text-blue-700 sm:text-red-700 md:text-gray-600 lg:text-yellow-500 xl:text-green-600 2xl:text-pink-400 3xl:text-purple-900">
+              <h2 className="text-2xl sm:text-3xl lg:text-4xl uppercase tracking-wider mt-8">
                 Explore Recipes
               </h2>
-              <p className="text-xl sm:text-2xl mt-8 px-10 pb-12 xs:pb-16 leading-relaxed">
+              <p className="text-xl sm:text-2xl lg:text-3xl mt-8 px-10 pb-12 xs:pb-16 leading-relaxed">
                 A collection of recipes inspired from the places I have visited.
               </p>
               {/* Button */}
               <Link to="/recipes">
               <button
-                onClick={() => console.log("Navigate to recipes page")} // Update navigation logic
-                className="w-[130px] h-[50px] px-2 py-2 mt-[-10px] bg-custom-accent text-white text-sm rounded-lg border-2 border-white hover:bg-opacity-90 whitespace-nowrap"
+                onClick={() => console.log("Navigate to recipes page")}
+                className="w-[130px] h-[50px] lg:w-[150px] lg:text-lg px-2 py-2 mt-[-10px] bg-custom-accent text-white text-sm rounded-lg border-2 border-white hover:bg-opacity-90 whitespace-nowrap"
               >
                 Explore Recipes
               </button>
