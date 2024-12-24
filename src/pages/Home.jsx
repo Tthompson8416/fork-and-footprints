b323ce7ce4a8f2ba9f5c269af8ecb67cc15cba8c
TT84 <tthompson8416@outlook.com>
2024-12-24 18:38:31 -0500

    Slide 2 styles adjusted for RWD

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -100,18 +100,18 @@ const Home = () => {
             </div>
 
             {/* Transparent Rectangle */}
-            <div className="absolute max-w-[80%] md:max-w-[60%] w-[900px] h-[300px] 3xl:h-[350px] top-72 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-50 border-4 border-gray-200 text-center rounded-lg xs:text-blue-700 sm:text-red-700 md:text-gray-600 lg:text-yellow-500 xl:text-green-600 2xl:text-pink-400 3xl:text-purple-900">
+            <div className="absolute max-w-[80%] w-[900px] h-[320px] md:max-w-[60%] 3xl:h-[350px] top-72 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-50 border-4 border-gray-200 text-center rounded-lg xs:text-blue-700 sm:text-red-700 md:text-gray-600 lg:text-yellow-500 xl:text-green-600 2xl:text-pink-400 3xl:text-purple-900">
               <h2 className="text-2xl sm:text-3xl lg:text-4xl 3xl:text-5xl uppercase tracking-wider mt-8">
                 Explore Recipes
               </h2>
-              <p className="text-xl sm:text-2xl lg:text-3xl 3xl:text-4xl mt-8 px-10 pb-12 xs:pb-16 leading-relaxed">
+              <p className="text-xl sm:text-2xl lg:text-3xl 3xl:text-4xl mt-8 md:mt-12 px-10 pb-12 xs:pb-8 leading-relaxed">
                 A collection of recipes inspired from the places I have visited.
               </p>
               {/* Button */}
               <Link to="/recipes">
               <button
                 onClick={() => console.log("Navigate to recipes page")}
-                className="w-[130px] h-[50px] lg:w-[150px] 3xl:w-[170px] 3xl:h-[60px] lg:text-lg 3xl:text-xl px-2 py-2 mt-[-10px] bg-custom-accent text-white text-sm rounded-lg border-2 border-white hover:bg-opacity-90 whitespace-nowrap"
+                className="w-[130px] h-[50px] xs:h-[40px] lg:w-[150px] 3xl:w-[170px] 3xl:h-[60px] lg:text-lg 3xl:text-xl sm:mt-4 lg:mt-8 lg:pb-8 3xl:pb-2 px-2 py-2 mt-[-10px] bg-custom-accent text-white text-sm rounded-lg border-2 border-white hover:bg-opacity-90 whitespace-nowrap"
               >
                 Explore Recipes
               </button>
