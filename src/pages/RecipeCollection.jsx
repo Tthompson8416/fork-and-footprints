c5614c7275c6a170f66c9622b066126a34afab66
TT84 <tthompson8416@outlook.com>
2024-12-26 04:13:24 -0500

    Hero image added to recipe page

--- src/pages/RecipeCollection.jsx
+++ src/pages/RecipeCollection.jsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css"; // Base Swiper styles
+import "swiper/css/pagination"; // Pagination styles
+import HeroImage from "../assets/images/recipes-hero.png";
+import forkImage from "../assets/images/fork.png";
+import { Link } from "react-router-dom";
+
+
 function RecipeCollection() {
   return (
-    <div>
-      <h2 className="text-3xl font-bold mb-4">Recipe Collection</h2>
+    <div className="bg-white w-screen h-screen">
+      <img src={HeroImage} alt="Hero Image" className="w-full h-auto max-h-[600px] object-contain mb-8 bg-white" />
       <p className="text-lg mb-4">Discover recipes inspired by global cuisines. From Italian pasta to Thai curry, find something to cook and enjoy!</p>
       <ul className="list-disc pl-5 space-y-2">
         <li>Classic French Ratatouille</li>
