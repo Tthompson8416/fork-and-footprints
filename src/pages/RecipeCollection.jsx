ab6648fb2de14accf2a357e39b3c8a4967c3fc67
TT84 <tthompson8416@outlook.com>
2024-12-27 22:58:16 -0500

    Dynamic css and clamp modified for cleaner code

--- src/pages/RecipeCollection.jsx
+++ src/pages/RecipeCollection.jsx
@@ -11,13 +11,12 @@ import HeroImage1 from "../assets/images/recipes-hero.png";
 const RecipeCollection = () => {
   return (
     <div className="bg-green-100 min-h-screen w-full overflow-hidden p-0">
-      <div>
-        <img
-          src={HeroImage1}
-          alt="recipes-text-on-cutting-board"
-          className="relative left-0 w-full h-[clamp(300px, 50vw, 550px)] object-contain md:h-[400px] md:object-cover"
-        />
-      </div>
+      <img
+        src={HeroImage1}
+        alt="recipes-text-on-cutting-board"
+        className="relative left-0 w-full h-[clamp(300px, 50vw, 550px)] object-contain sm:h-[270px] sm:object-cover sm:mt-8 md:object-cover md:mt-12 lg:mt-16 xl:h-[320px] 2xl:h-[375px] 3xl:h-[550px]"
+      />
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-0 3xl:mt-36 mb-24 p-4">
         {countries.map((country) => (
           <CountryCard
