1ddd0633c82ec56ffbd922cccb1a87130cd25984
TT84 <tthompson8416@outlook.com>
2024-12-29 13:10:42 -0500

    404.html file removed and vite.config.js file reverted to original code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://tthompson8416.github.io/fork-and-footprints/'
+  base: '/fork-and-footprints/', 
 });
