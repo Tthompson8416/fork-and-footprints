8c79634ff5f527b8d121f84d05c48590496f004d
TT84 <tthompson8416@outlook.com>
2024-12-29 14:34:29 -0500

    Added all recipe imports to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,39 @@ import WorldMap from "./pages/WorldMap";
 import CountryDetail from "./pages/CountryDetail";
 import RecipeCollection from "./pages/RecipeCollection";
 import Recipe1 from "./pages/Recipe1";
+import Recipe1 from "./pages/Recipe2";
+import Recipe1 from "./pages/Recipe3";
+import Recipe1 from "./pages/Recipe4";
+import Recipe1 from "./pages/Recipe5";
+import Recipe1 from "./pages/Recipe6";
+import Recipe1 from "./pages/Recipe7";
+import Recipe1 from "./pages/Recipe8";
+import Recipe1 from "./pages/Recipe9";
+import Recipe1 from "./pages/Recipe10";
+import Recipe1 from "./pages/Recipe11";
+import Recipe1 from "./pages/Recipe12";
+import Recipe1 from "./pages/Recipe13";
+import Recipe1 from "./pages/Recipe14";
+import Recipe1 from "./pages/Recipe15";
+import Recipe1 from "./pages/Recipe16";
+import Recipe1 from "./pages/Recipe17";
+import Recipe1 from "./pages/Recipe18";
+import Recipe1 from "./pages/Recipe19";
+import Recipe1 from "./pages/Recipe20";
+import Recipe1 from "./pages/Recipe21";
+import Recipe1 from "./pages/Recipe22";
+import Recipe1 from "./pages/Recipe23";
+import Recipe1 from "./pages/Recipe24";
+import Recipe1 from "./pages/Recipe25";
+import Recipe1 from "./pages/Recipe26";
+import Recipe1 from "./pages/Recipe27";
+import Recipe1 from "./pages/Recipe28";
+import Recipe1 from "./pages/Recipe29";
+import Recipe1 from "./pages/Recipe30";
+import Recipe1 from "./pages/Recipe31";
+import Recipe1 from "./pages/Recipe32";
+import Recipe1 from "./pages/Recipe33";
+
 
 
 function App() {
