5aa3a2fb223715a5131cf6fcc42634fcf0ad34a0
TT84 <tthompson8416@outlook.com>
2024-12-28 19:55:39 -0500

    11 sections added to RecipesPage

--- src/pages/RecipesPage.jsx
+++ src/pages/RecipesPage.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const RecipesPage = () => {
-    return (
-        <div className='bg-white min-h-screen'>
+  const countries = [
+    { id: 1, name: "Belgium" },
+    { id: 2, name: "Belize" },
+    { id: 3, name: "Canada" },
+    { id: 4, name: "Croatia" },
+    { id: 5, name: "France" },
+    { id: 6, name: "Germany" },
+    { id: 7, name: "Italy" },
+    { id: 8, name: "Mexico" },
+    { id: 9, name: "Netherlands" },
+    { id: 10, name: "Portugal" },
+    { id: 11, name: "Spain" },
+  ];
 
+  return (
+    <div className="bg-white min-h-screen">
+      {countries.map((country, index) => (
+        <div key={index} className="py-16 px-8">
+          <h2 className="text-2xl font-bold text-black text-center">
+            {country}
+          </h2>
+          {index < countries.length - 1 && (
+            <div className="border-t border-gray-300 my-8"></div>
+          )}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default RecipesPage;
-
-
