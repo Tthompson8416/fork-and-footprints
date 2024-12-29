146657e1a1c79a3208b6954839599be6e70e8299
TT84 <tthompson8416@outlook.com>
2024-12-29 14:18:43 -0500

    url path updated on CountryCards

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import WorldMap from "./pages/WorldMap";
 import CountryDetail from "./pages/CountryDetail";
 import RecipeCollection from "./pages/RecipeCollection";
+import Recipe1 from "./pages/Recipe1";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/worldmap" element={<WorldMap />} />
           <Route path="/recipes" element={<RecipeCollection />} />
+          <Route path="/recipes/recipe1" element={<Recipe1 />} />
           <Route path="/country/:id" element={<CountryDetail />} />
         </Routes>
       </Layout>
--- src/pages/CountryCard.jsx
+++ src/pages/CountryCard.jsx
@@ -1,17 +1,16 @@
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const CountryCard = ({ name, image, pageLink, aosAnimation }) => {
-
   const handlebuttonClick = () => {
     const url = `/recipes/country#${name.toLowerCase()}`;
     window.open(url, "_blank");
   };
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-4 pb-12 mb-12 max-w-[clamp(280px, 90%, 350px)] mx-auto"
+    <div
+      className="bg-white shadow-lg rounded-lg p-4 pb-12 mb-12 max-w-[clamp(280px, 90%, 350px)] mx-auto"
       data-aos={aosAnimation}
-      >
+    >
       <img
         src={image}
         alt={name}
@@ -20,12 +19,12 @@ const CountryCard = ({ name, image, pageLink, aosAnimation }) => {
       <h1 className="text-[clamp(1rem, 2.5vw, 2.5rem)] font-bold font-serif mt-4 text-center text-custom-accent text-[1.5rem] sm:text-[1.7rem] lg:text-[2rem]">
         {name}
       </h1>
-      
+
       <button
         className="mt-8 px-[clamp(0.5rem, 1.5vw, 1rem)] py-[clamp(0.3rem, 1vw, 0.5rem)] py-[1rem] px-[2rem] bg-blue-400 text-white text-xl xl:text-2xl rounded hover:bg-blue-600 mx-auto block"
-        onClick={handlebuttonClick}
+        onClick={() => (window.location.href = `/fork-and-footprints/recipes/recipe1`)}
       >
-        Learn More
+        Recipe 1
       </button>
     </div>
   );
