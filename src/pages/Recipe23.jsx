a42fbd4ff78198d0688fa9aec7cd1e73a1ffd05b
TT84 <tthompson8416@outlook.com>
2024-12-29 13:41:41 -0500

    33 files created with basic setup for recipes

---
+++ src/pages/Recipe23.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Recipe23 = () => {
+  return (
+    <div>
+      <h1>Recipe 1: Dish Name</h1>
+      <p>Details about the recipe...</p>
+    </div>
+  );
+};
+
+export default Recipe23;
